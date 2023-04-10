0292380f9a5c5c9f95220baa7b3a093741feb208
Zeltzin Romero <zeltzin.rom96@gmail.com>
2023-04-10 07:51:19 -0600

    Maquetación

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+console.log(data.films);
+console.log(data.films[0].title);
+
+
+const dataFilms = data.films;
+
+console.log(dataFilms.map(film => film.title))
+
+
+const nav_history = document.querySelector('.nav-history');
+const modal = document.getElementById('modal')
+
+function mostrarModal() {
+
+  console.log('esatas dentro')
+  modal.showModal()
+
+}
+
+nav_history.addEventListener('click', mostrarModal())
+
+
+
